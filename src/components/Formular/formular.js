79bd16c496b35df8ae75990e91e3f08599fac636
jsango2 <jure.sango@gmail.com>
2022-05-05 16:46:01 +0200

    all

--- src/components/Formular/formular.js
+++ src/components/Formular/formular.js
@@ -24,37 +24,24 @@ const Formular = () => {
     <WrapFormSection ref={ref}>
       <Photo inView={inView} />
       <Rectangle inView={inView} />
-      {/* <form
-        name="contact"
+
+      <Form
+        inView={inView}
+        name="contact forma"
+        method="post"
         data-netlify="true"
         data-netlify-honeypot="bot-field"
-        hidden
+        action="/success"
       >
-        <input type="hidden" name="form-name" value="contact" />
-        <input type="text" name="ime" />
-        <input type="email" name="email" />
-        <input type="text" name="imeObjekta" />
-        <input type="tel" name="brojTelefona" />
-        <input type="text" name="mjesto" />
-        <input type="text" name="sustav" />
-        <input type="number" name="brojJedinica" />
-      </form> */}
-
-      <Form inView={inView}>
         {size.width > 440 ? (
-          <form
-            name="contact"
-            method="post"
-            data-netlify="true"
-            data-netlify-honeypot="bot-field"
-          >
-            <input type="hidden" name="form-name" value="contact" />
+          <>
+            <input type="hidden" name="form-name" value="contact forma" />
 
             <input type="text" name="Ime" placeholder="Ime" />
             <input type="email" name="Email" placeholder="Email" />
             <textarea name="Text" rows="5" cols="33" placeholder="Poruka" />
             <button type="submit">POŠALJI UPIT</button>
-          </form>
+          </>
         ) : (
           <WrapFormMob>
             <Title>Obratite nam se s povjerenjem</Title>
